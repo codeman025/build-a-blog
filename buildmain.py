e00e1878b3eff2cf6b90b33b27eb2db72e90a64e
mk <cody.long.cl21@gmail.com>
2017-07-13 15:57:02 -0700

    first commit

---
+++ buildmain.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:1234@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+
+    def __init__(self, name):
+        self.name = name
+
+tasks = []
+
+@app.route('/',methods=['POST', 'GET'])
+def index():
+
+    if request.method == 'POST':
+        task = request.form['task']
+        tasks.append(task)
+
+    return render_template('todos.html',title="build-a-blog",tasks=tasks)
+
+if __name__ == '__main__':
+    app.run()
